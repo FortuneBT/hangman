6706cdf9f93899e4cd87d5c0dff56878693f9f93
FortuneBT <fortune.tshali@gmail.com>
2022-01-26 10:54:03 +0100

    update the method init in class hangman

--- utils/game.py
+++ utils/game.py
@@ -1,4 +1,6 @@
+from ssl import HAS_ALPN
 from typing import List
+from random import Random, randint
 
 class Hangman():
 
@@ -13,10 +15,36 @@ class Hangman():
         self.wrongly_guessed_letters:List[str] = []
         self.turn_count:int = 0
         self.error_count:int = 0
+
+        #select a randomize choice of word in the list (possible_words)
+        choice:int = randint(0,len(self.possible_words)-1)
+
+        #to create the variable word_to_find
+        for letter in self.possible_words[choice]:
+            self.word_to_find.append(letter)
+
+        #allow to have the exact number of underscore
+        self.correctly_guessed_letters = []
+        for letter in self.word_to_find:
+            self.correctly_guessed_letters.append("_")
         
+        print(self.correctly_guessed_letters)
 
     def play(self):
-        pass
+        """
+        method that asks the player to enter a letter. The player shouldn't be 
+        allowed to type something else than a letter, and not more than a letter. If the player
+        guessed a letter well, add it to the well_guessed_letters list. If not, i will add it 
+        to the wrongly_guessed_letters list and add 1 to error_count.
+        """
+        playerEntry = input("Let's play! You're allow to enter one character : ")
+
+        #this loop will continue until there is only one characte 
+        while len(playerEntry) != 1:
+            playerEntry = input("You're only allow to enter one character")
+        
+        
+
 
     def start_game(self):
         """
@@ -40,3 +68,6 @@ class Hangman():
         Method that will print the positive message when we win
         """
         print(f"You found the word : {self.word_to_find}")
+
+
+pendu = Hangman()
